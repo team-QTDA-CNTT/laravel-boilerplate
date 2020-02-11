3d0a24383e4e030cef6ea32aa25efc622978b8a4
lethai2597 <lethai2597@gmail.com>
2020-02-11 10:02:38 +0700

    x

--- resources/views/doc.blade.php
+++ resources/views/doc.blade.php
@@ -1651,9 +1651,9 @@
                 <div class="m-3">
                   <h3 class="mb-4"><span class="font-bold">SINGLE SLIDER</span> <span class="text-sm">OWL PLUGIN</span></h3>
 
-                  <div class="relative overflow-hidden carousel1">
-                    <div class="float-left transition duration-1000 ease-in-out carousel-inner">
-                      <div class="float-left carousel-item" style="display: none;">
+                  <div class="relative overflow-hidden">
+                    <div class="float-left transition duration-1000 ease-in-out">
+                      <div class="float-left" style="display: none;">
                         <img class="w-full" src="http://bootstrap-ecommerce.com/bootstrap-ecommerce-html/images/banners/slide1.jpg" alt="">
                       </div>
                       <div class="float-left carousel-item">
@@ -1675,10 +1675,10 @@
               <div class="w-full lg:w-1/2">
                 <div class="m-2 ">
                   <h3 class="mb-4"><span class="font-bold">SINGLE SLIDER</span> <span class="font-sm">BOOTSTRAP</span></h3>
-                  <div class="relative overflow-hidden carousel2">
-                    <img class="hidden w-full slide" style="display: block;" src="http://bootstrap-ecommerce.com/bootstrap-ecommerce-html/images/banners/slide1.jpg" alt="">
-                    <img class="hidden w-full slide" src="http://bootstrap-ecommerce.com/bootstrap-ecommerce-html/images/banners/slide2.jpg" alt="">
-                    <img class="hidden w-full slide" src="http://bootstrap-ecommerce.com/bootstrap-ecommerce-html/images/banners/slide3.jpg" alt="">
+                  <div class="relative overflow-hidden">
+                    <img class="hidden w-full" style="display: block;" src="http://bootstrap-ecommerce.com/bootstrap-ecommerce-html/images/banners/slide1.jpg" alt="">
+                    <img class="hidden w-full" src="http://bootstrap-ecommerce.com/bootstrap-ecommerce-html/images/banners/slide2.jpg" alt="">
+                    <img class="hidden w-full" src="http://bootstrap-ecommerce.com/bootstrap-ecommerce-html/images/banners/slide3.jpg" alt="">
                     <div onclick="" class="absolute top-0 left-0 z-50 flex items-center justify-center w-1/6 h-full text-5xl text-white opacity-75 cursor-pointer hover:opacity-100">
                       <i class="fas fa-chevron-left"></i>
                     </div>
@@ -1686,9 +1686,9 @@
                       <i class="fas fa-chevron-right"></i>
                     </div>
                     <div class="absolute bottom-0 w-full mb-4 text-center">
-                      <span class="inline-block w-3 h-3 mx-2 border border-white rounded-full dot"></span>
-                      <span class="inline-block w-3 h-3 mx-2 border border-white rounded-full dot"></span>
-                      <span class="inline-block w-3 h-3 mx-2 border border-white rounded-full dot"></span>
+                      <span class="inline-block w-3 h-3 mx-2 border border-white rounded-full"></span>
+                      <span class="inline-block w-3 h-3 mx-2 border border-white rounded-full"></span>
+                      <span class="inline-block w-3 h-3 mx-2 border border-white rounded-full"></span>
                     </div>
                   </div>
                 </div>
